feaf473df00f48108242aa3cbf9a450d94601214
Ramon Vieira <ramonvieira122333@gmail.com>
2022-08-31 03:43:11 -0300

    first progress in the logic of the calculation of the BMI

--- scripts/modules/app.js
+++ scripts/modules/app.js
@@ -1,18 +1,44 @@
 export default function calculateImc() {}
 
-// DESCOBRIR COMO PEGAR ESSE VALOR AQUI
-const sex = document.querySelectorAll("input[name='sex']");
-
+const genero = document.querySelectorAll("input[name=genero]");
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
-
 const button = document.getElementById("btn");
 
 button.addEventListener("click", calculate);
 
 function calculate() {
-  console.log(weight.value, height.value);
   const imc = weight.value / (height.value * height.value);
 
+  // PEGANDO A DESGRAÇA DO VALUE DO RADIO MARCADO
+  const valueGenero = [];
+  for (let index = 0; index < genero.length; index++) {
+    if (genero[index].checked) {
+      valueGenero.push(genero[index].value);
+    }
+  }
+
   console.log(imc);
+
+  if (valueGenero[0] === "man") {
+    console.log(valueGenero[0]);
+    if (imc < 17.3) {
+      console.log("MAGREZA");
+    } else if (imc > 17.3 && imc < 25.5) {
+      console.log("NORMAL");
+    } else if (imc > 25.5) {
+      console.log("SOBREPESO");
+    }
+  } else {
+    console.log(valueGenero[0]);
+    if (imc < 18.5) {
+      console.log("MAGREZA");
+    } else if (imc > 18.5 && imc < 24.9) {
+      console.log("NORMAL");
+    } else if (imc > 24.9) {
+      console.log("SOBREPESO");
+    }
+  }
+
+  console.log(valueGenero);
 }
