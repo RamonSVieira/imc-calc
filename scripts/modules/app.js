63c440c5c3e26c3335851b7bb52ba8b60c8dd3d7
Ramon Vieira <ramonvieira122333@gmail.com>
2022-09-02 01:24:55 -0300

    change in texts

--- scripts/modules/app.js
+++ scripts/modules/app.js
@@ -36,25 +36,24 @@ export default function calculateImc() {
 
       if (valueGenero[0] === "man") {
         if (imc < 17.3) {
-          txtInfoImc.innerText = "MAGO PRA KARA***";
+          txtInfoImc.innerText = "VOCÊ ESTÁ ABAIXO DO PESO";
           imageImc.src = "./images/slim-man.png";
         } else if (imc > 17.3 && imc < 25.5) {
-          txtInfoImc.innerText = "POR** TU TREINA É BOE?";
+          txtInfoImc.innerText = "VOCÊ ESTÁ COM PESO IDEAL";
           imageImc.src = "./images/fit-man.png";
         } else if (imc > 25.5) {
-          txtInfoImc.innerText =
-            "TA ME OLHANDO PORQUE? TA PENSANDO QUE EU SOU UM SACO DE DORITOS É GORDOLA?";
+          txtInfoImc.innerText = "VOCÊ ESTÁ ACIMA DO PESO IDEAL";
           imageImc.src = "./images/fat-man.png";
         }
       } else {
         if (imc < 18.5) {
-          txtInfoImc.innerText = "SÓ O PALITO";
+          txtInfoImc.innerText = "VOCÊ ESTÁ ABAIXO DO PESO";
           imageImc.src = "./images/thin-woman.png";
         } else if (imc > 18.5 && imc < 24.9) {
-          txtInfoImc.innerText = "ESSA É RATA DE ACADEMIA";
+          txtInfoImc.innerText = "VOCÊ ESTÁ COM PESO IDEAL";
           imageImc.src = "./images/fit-woman.png";
         } else if (imc > 24.9) {
-          txtInfoImc.innerText = "KRAI, TA MAIS GORDA QUE A MÃE DE JADSON";
+          txtInfoImc.innerText = "VOCÊ ESTÁ ACIMA DO PESO IDEAL";
           imageImc.src = "./images/fat-woman.png";
         }
       }
